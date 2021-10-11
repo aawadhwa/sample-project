e2ffc89828923ee12876ed1845516a53747c294c
Abhishek <user@mysite.com>
2021-10-11 00:56:43 -0400

    feat - adding swagger docs

--- handler.js
+++ handler.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+
 const app = express();
 
 app.use(express.json());
+const YAML = require('yamljs');
+const swaggerdoc = path.resolve(__dirname, './apis/openapi.yaml');
+const swaggerDocument = YAML.load(swaggerdoc);
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 const userController = require('./apis/controllers/user-controller')
 
 app.use('/users',userController);
